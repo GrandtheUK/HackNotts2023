8836cb37e6ec2e52a3ca776b27a3d74dc05d036d
Benjamin (N1022294) <31855376+GrandtheUK@users.noreply.github.com>
2023-02-11 13:39:32 +0000

    Basic functionality achieved (it shows up)

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,27 @@
-use bevy::prelude::*;
+use bevy::{prelude::*, render::camera};
+
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn(Camera2dBundle::default());
+    commands.spawn(
+        SpriteBundle {
+            texture: asset_server.load("Sprite-0001.png"),
+            transform: Transform::from_xyz(-100., -100., 0.),
+            ..default()
+        }
+    );
+    commands.spawn(
+        SpriteBundle {
+            texture: asset_server.load("Sprite-0001.png"),
+            transform: Transform::from_xyz(100., 100., 0.),
+            ..default()
+        }
+    );
+}
+
 
 fn main() {
     App::new()
+        .add_startup_system(setup)
         .add_plugins(DefaultPlugins)
         .run();
 }
